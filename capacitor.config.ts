fa8e2b6275724ffaeb798d047b1c16c9a4db7746
NiepelH <info@sheep.cloud>
2022-11-14 13:31:23 +0100

    ionic init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ltv.010',
+  appName: 'ltv-010',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
